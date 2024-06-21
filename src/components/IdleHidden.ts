624b3fef4993438d4f887cd81b612eec3de7791f
NewYearNewPhil <NewYearNewPhil>
2024-06-22 00:44:44 +0200

    improves buttons

---
+++ src/components/IdleHidden.ts
@@ -0,0 +1,30 @@
+export default function init() {
+    document.addEventListener("DOMContentLoaded", function () {
+        let idleTimeout: number | undefined;
+
+        function resetIdleTimer() {
+            clearTimeout(idleTimeout);
+            showIdleElements();
+            idleTimeout = setTimeout(hideIdleElements, 2500);
+        }
+
+        function hideIdleElements() {
+            const elements = document.querySelectorAll('.idle-hidden');
+            elements.forEach(el => {
+                (el as HTMLElement).style.opacity = '0';
+            });
+        }
+
+        function showIdleElements() {
+            const elements = document.querySelectorAll('.idle-hidden');
+            elements.forEach(el => {
+                (el as HTMLElement).style.opacity = '1';
+            });
+        }
+
+        resetIdleTimer();
+        ['scroll', 'click', 'touchstart', 'touchend', 'mousemove'].forEach(event => {
+            window.addEventListener(event, resetIdleTimer, true);
+        });
+    });
+}
